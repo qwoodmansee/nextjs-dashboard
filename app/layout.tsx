347cbb6d0a716d077d979da384711ed1328c68ac
Quinton Woodmansee <qwoodmansee@capital.edu>
2024-02-09 15:35:09 -0500

    chapter 2 - tailwind and css modules

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+/* example from chapter 2 showing you can do both tailwind and css modules */
+import '@/app/ui/global.css'; // tailwind, modules can be found in app/page.tsx + app/ui/home.module.css
+
 export default function RootLayout({
   children,
 }: {
