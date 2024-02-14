fead99c0cef569e13cdbe7f822903fda1b2cac3e
Quinton Woodmansee <qwoodmansee@capital.edu>
2024-02-14 09:30:10 -0500

    chapter 13, handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,42 +17,66 @@ const CreateInvoiceSchema = FormSchema.omit({ id: true, date: true });
 const UpdateInvoiceSchema = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const invoiceToCreate = CreateInvoiceSchema.parse(Object.fromEntries(formData.entries()));
-  const amountInCents = invoiceToCreate.amount * 100;
-  const nowAsDate = new Date().toISOString().split('T')[0];
+  const { customerId, amount, status } = CreateInvoiceSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${invoiceToCreate.customerId}, ${amountInCents}, ${invoiceToCreate.status}, ${nowAsDate})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${ customerId }, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
  
   revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
 
+  //NOTE(quinton): according to docs, you have to do the redirect outside of try bc rediect throws an error as an implementation detail kek
+  redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const invoiceToUpdate = UpdateInvoiceSchema.parse(Object.fromEntries(formData.entries()));
-  const amountInCents = invoiceToUpdate.amount * 100;
-
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${invoiceToUpdate.customerId},
-        amount = ${amountInCents},
-        status = ${invoiceToUpdate.status}
-    WHERE id = ${id}
-  `;
+  const { customerId, amount, status } = UpdateInvoiceSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
-
 }
 
 export async function deleteInvoice(id: string) {
 
-  await sql`
-    DELETE FROM invoices WHERE id = ${id}
-  `;
- 
-  revalidatePath('/dashboard/invoices');
+  //FIXME(quinton): remove this before committing
+  throw new Error('Failed to Delete Invoice');
+
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }  
 }
